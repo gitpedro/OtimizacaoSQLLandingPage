b2ee0e7efd481879600a546f417b074a9cf63926
Pedro F <pfernandes80@yahoo.com>
2024-08-22 01:59:05 -0300

    Exemplo criação de landing page usando WebForms com Claude IA 3.5 gratis

---
+++ OtimizacaoSQLLandingPageWebForms/Otimizacao.aspx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.UI;
+
+namespace OtimizacaoSQLLandingPageWebForms
+{
+    public partial class Otimizacao : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        protected void AnalisarBtn_Click(object sender, EventArgs e)
+        {
+            string consulta = ConsultaInput.Text;
+
+            // Aqui você implementaria a lógica real de análise da consulta
+            // Por enquanto, vamos usar resultados mockados
+            List<string> sugestoes = new List<string>
+            {
+                "Criar índice composto",
+                "Usar hint /*+ INDEX */"
+            };
+
+            string tempoOriginal = "2.5s";
+            string tempoOtimizado = "0.5s";
+
+            // Exibir resultados
+            SugestoesList.DataSource = sugestoes;
+            SugestoesList.DataBind();
+
+            DesempenhoLiteral.Text = $"Tempo Original: {tempoOriginal}<br>Tempo Otimizado: {tempoOtimizado}";
+
+            ResultadoPanel.Visible = true;
+        }
+
+        protected void SolicitarDemoBtn_Click(object sender, EventArgs e)
+        {
+            // Implementar lógica para solicitar demo
+            Response.Write("<script>alert('Solicitação de demo enviada!');</script>");
+        }
+    }
+}
---
+++ OtimizacaoSQLLandingPageWebForms/Otimizacao.aspx.designer.cs
@@ -0,0 +1,71 @@
+﻿//------------------------------------------------------------------------------
+// <gerado automaticamente>
+//     Esse código foi gerado por uma ferramenta.
+//
+//     As alterações ao arquivo poderão causar comportamento incorreto e serão perdidas se
+//     o código for recriado
+// </gerado automaticamente>
+//------------------------------------------------------------------------------
+
+namespace OtimizacaoSQLLandingPageWebForms
+{
+
+
+    public partial class Otimizacao
+    {
+
+        /// <summary>
+        /// Controle ConsultaInput.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox ConsultaInput;
+
+        /// <summary>
+        /// Controle AnalisarBtn.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button AnalisarBtn;
+
+        /// <summary>
+        /// Controle ResultadoPanel.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel ResultadoPanel;
+
+        /// <summary>
+        /// Controle SugestoesList.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.BulletedList SugestoesList;
+
+        /// <summary>
+        /// Controle DesempenhoLiteral.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Literal DesempenhoLiteral;
+
+        /// <summary>
+        /// Controle SolicitarDemoBtn.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button SolicitarDemoBtn;
+    }
+}
